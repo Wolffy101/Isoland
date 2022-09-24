e2f76ae9b1d2a0c0b4043d558d766af6e91f1ee5
高勇 <gaoyong101@outlook.com>
2022-09-24 18:08:18 +0800

    修改为和gdscrpit 一样的方式

--- Globals/SceneChanger.cs
+++ Globals/SceneChanger.cs
@@ -6,12 +6,13 @@ namespace Isoland.Globals;
 /// </summary>
 public partial class SceneChanger : CanvasLayer
 {
-    private static ColorRect _colorRect;
     /// <summary>
     /// 当前类的实列 
     /// </summary>
-    public static SceneChanger Singleton { get; private set; }
+    private static SceneChanger Singleton;
 
+    private ColorRect _colorRect;
+    public static ColorRect ColorRect => Singleton._colorRect;
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
@@ -24,13 +25,13 @@ public partial class SceneChanger : CanvasLayer
     public override void _Process(double delta)
     {
     }
-    public void ChangeSceneToFile(string path)
+    public static void ChangeSceneToFile(string path)
     {
-        var tween = CreateTween();
-        tween.TweenCallback(_colorRect.Show);
-        tween.TweenProperty(_colorRect, "color:a", 1.0f, 0.2f);
-        tween.TweenCallback(() => GetTree().ChangeSceneToFile(path));
-        tween.TweenProperty(_colorRect, "color:a", 0f, 0.3f);
-        tween.TweenCallback(_colorRect.Hide);
+        var tween = Singleton.CreateTween();
+        tween.TweenCallback(ColorRect.Show);
+        tween.TweenProperty(ColorRect, "color:a", 1.0f, 0.2f);
+        tween.TweenCallback(() => Singleton.GetTree().ChangeSceneToFile(path));
+        tween.TweenProperty(ColorRect, "color:a", 0f, 0.3f);
+        tween.TweenCallback(ColorRect.Hide);
     }
 }
